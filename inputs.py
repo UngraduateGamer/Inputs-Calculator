26b8fd135f64c76a23eeb784ca6b5209f186ede6
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-15 16:05:12 +0530

    Create inputs.py
    in this python file we use inputs to create a simple calculator

---
+++ inputs.py
@@ -0,0 +1,9 @@
+name=input("Enter your Name: ")
+Age=int(input("Enter your Age: "))
+num1=int(input("Enter First Number: "))
+num2=int(input("Enter Second Number: "))
+
+print("The Value of ",num1,"+",num2,"is:-",num1+num2)
+print("The Value of ",num1,"-",num2,"is:-",num1-num2)
+print("The Value of ",num1,"/",num2,"is:-",num1/num2)
+print("The Value of ",num1,"*",num2,"is:-",num1*num2)
